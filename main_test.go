ffe49590ad3854f64f9d48881c1391c4635caf8e
Jepasq <jepasq111@gmail.com>
2023-02-20 06:41:44 +0100

    Start to implement hello page test using a request recorder

--- main_test.go
+++ main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 	"regexp"
+	"net/http"
+	"net/http/httptest"
 )
 
 /// intro text copntains 'Welcome' word
@@ -11,6 +13,33 @@ func TestIntroWelcome(t *testing.T) {
 	want := regexp.MustCompile(`\b`+name+`\b`)
 	msg := getIntroText()
 	if !want.MatchString(msg) {
-		t.Fatalf(`Hello("Gladys") = %q, want match for %#q, nil`, msg, want)
+		t.Fatalf(`Can't find Welcome text =
+%q, want match for %#q, nil`, msg, want)
 	}
+}
+
+/// We can render correctly the hello page.
+/// see https://blog.questionable.services/article/testing-http-handlers-go/
+func TestHelloPageRendering(t *testing.T) {
+	name := "HELLO"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	
+	// Create a request to pass to our handler. We don't have any query
+	// parameters for now, so we'll pass 'nil' as the third parameter.
+	_ /*req*/ , err := http.NewRequest("GET", "/hello", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter)
+	// to record the response.
+	rr := httptest.NewRecorder()
+	
+	t.Fatalf(`HELLO page rendering failed %q`, want,)
+
+	/*
+	if !want.MatchString(msg) {
+		t.Fatalf(`HELLO page rendering failed`)
+	}
+	*/
 }
