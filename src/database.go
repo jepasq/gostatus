d74065bacd9efc19294d6f33d494d0556c99cfa4
Jepasq <jepasq111@gmail.com>
2024-07-26 08:46:52 +0200

    Add Database type and Connect function

--- src/database.go
+++ src/database.go
@@ -5,7 +5,15 @@ package main
 import (
 	"os"
 	"log"
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Database struct {
+	Client *mongo.Client
+}
 
 func CheckConnString() {
 	uri := os.Getenv("MONGODB_URI")
@@ -14,4 +22,26 @@ func CheckConnString() {
 			"See: " +
 			"www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
+}
+
+func Connect() (Database) {	
+	ret := Database{}
+
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	ret.Client = client
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	return ret
 }
