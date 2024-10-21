bc1940936129ac44fa37055c0e3276efa8ae82f9
Jepasq <jepasq111@gmail.com>
2024-10-21 11:29:30 +0200

    Trying to move addListener JS callback to a satic file

---
+++ static/admin.js
@@ -0,0 +1,17 @@
+function addListenerCallback {
+    let nameTxt = $('#listenerAddId').val();
+    let typeTxt = $('#listenerType').find(":selected").val();
+    if (!nameTxt) {
+        alert("ID can't be empty.");
+        return
+    }
+    $.ajax({
+	url: "http://localhost:3333/listener/add",
+	data: { name: nameTxt, type: typeTxt },
+	method: "GET",
+	success: function(data) {
+	    // $("#response").html(data);
+	    alert("Success");
+	},
+    });
+}
