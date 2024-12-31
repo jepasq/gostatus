1a8db3fbe0f42fef094416a8692c458fcb758de3
Jepasq <jepasq111@gmail.com>
2024-12-31 11:02:37 +0100

    Switch to revision 15
    - Pass connected db via Context.WithValue
    - Fix missingh parenthesis in addListenerCallback definition
    - Finally fix the static/dir handling issue
    - Upgrade go-spew snappy go-cmp compress mongo-driver x/* and more
    - Documenting 404 error on JS and CSS static/ files
    - Fix curl contact URL in admin page
    - Handle help command line argument before checking db connection
    - Upgrade several x dependencies

--- src/main.go
+++ src/main.go
@@ -28,7 +28,7 @@ var templateDirs = []string{"../content", "../content/partials"}
 func getIntroText() (string) {
 	programName := os.Args[0]
 
-	return "Welcome to gostatus v0.0.1-14 (" + programName + ")"
+	return "Welcome to gostatus v0.0.1-15 (" + programName + ")"
 }
 
 /// Return a configuration directory (~/.gostatus)  content
